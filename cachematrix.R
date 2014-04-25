53299578678026ecbacb50f60ab829754c93687d
jaiabhayk <jai.abhayk@gmail.com>
2014-04-25 22:05:13 +0530

    Implement functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,8 @@
 # makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 # 
 # cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-# If the inverse has already been calculated (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
+# If the inverse has already been calculated (and the matrix has not changed), 
+# then the cachesolve retrieves the inverse from the cache.
 
 
 ###############################################
